59c5a919dbe727b235a4da63607f94a71c906374
Rubén Cuervo Madrigal <jrmcuervom@gmail.com>
2023-02-01 10:22:13 -0600

    Added local links in html file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const nav = document.getElementById('nav');
 const dh = document.getElementById('dh');
 const close = document.getElementById('close');
+
 close.addEventListener('click', () => {
   dh.classList.toggle('open');
 });
 
-nav.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {
   dh.classList.toggle('open');
 });
