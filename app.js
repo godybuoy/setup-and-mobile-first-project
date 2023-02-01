b68fa137c68f3fd331bb57e29db9ca9c1c28b72d
unknown <you@example.com>
2023-02-01 23:11:30 +0300

    update files

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const nav = document.getElementById('nav');
 const dh = document.getElementById('dh');
 const close = document.getElementById('close');
+const portfolio = document.getElementById('portfolio');
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
 
 close.addEventListener('click', () => {
   dh.classList.toggle('open');
 });
 
 hamburger.addEventListener('click', () => {
+  dh.classList.toggle('open');
+});
+portfolio.addEventListener('click', () => {
+  dh.classList.toggle('open');
+});
+about.addEventListener('click', () => {
+  dh.classList.toggle('open');
+});
+contact.addEventListener('click', () => {
   dh.classList.toggle('open');
 });
