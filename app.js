8d6224c8153a5b48763ffb9ac520ea319d97dad9
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-02-10 15:27:00 -0600

    Fix eslint errors

--- app.js
+++ app.js
@@ -60,34 +60,39 @@ for (let i = 0; i < description.length; i += 2) {
   description[i + 1].innerHTML = storeData[i + 1].description;
 }
 
-const projectLanguagesHtmlText = document.querySelectorAll('.languages-html');
-for (let i = 0; i < projectLanguagesHtmlText.length; i++) {
+/* const projectLanguagesHtmlText = document.querySelectorAll('.languages-html');
+for (let i = 0; i < projectLanguagesHtmlText.length; i += 1) {
   projectLanguagesHtmlText[i].innerHTML = storeData[0].technologies[0];
 }
 
-const projectLanguagesText = document.querySelectorAll('.languages-');
+/* const projectLanguagesText = document.querySelectorAll('.languages-');
 for (let i = 0; i < projectLanguagesText.length; i += 2) {
   projectLanguagesText[i].innerHTML = storeData[0].technologies[1];
   projectLanguagesText[i + 1].innerHTML = storeData[0].technologies[2];
-}
+} */
 
 // ..............popups...................
 
 const popups = document.querySelector('.popups');
 const popupProjectTitle = document.querySelector('.popup-project-title');
 const popupProjectDescription = document.querySelector('description');
-function popupsDisappearButton() {
+
+const disappearButton = document.querySelector('.popups-project-disapper-button');
+
+disappearButton.addEventListener('click', () => {
   popups.style.display = 'none';
-}
+});
 
 const popupsBootstrapLanguageDiv = document.querySelector('.bootstrap-language');
 const popupsRubyLanguageDiv = document.querySelector('.ruby-language');
 const popupsProjectDesktop = document.querySelector('.popups-project-p-desktop');
 const popupsProjectMobile = document.querySelector('.popups-project-p-mobile');
 const seeliveaTag = document.querySelector('.see-live-aTag');
-const popupsProjectImage = document.querySelector('#popups-project-img');
+// const popupsProjectImage = document.querySelector('#popups-project-img');
 
-function projectViewButton1() {
+const btnOne = document.querySelector('.btn-one');
+
+btnOne.addEventListener('click', () => {
   popups.style.display = 'flex';
   document.querySelector('#popups-project-img').src = storeData[0].imagesrc;
   popupProjectTitle.innerHTML = storeData[0].name;
@@ -96,29 +101,38 @@ function projectViewButton1() {
   seeliveaTag.href = storeData[0].href;
   popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
   popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
-}
+});
 
-function projectViewButton2() {
+const btnTwo = document.querySelector('.btn-two');
+
+btnTwo.addEventListener('click', () => {
   popups.style.display = 'flex';
   document.querySelector('#popups-project-img').src = storeData[1].imagesrc;
   popupProjectTitle.innerHTML = storeData[1].name;
-  popupProjectDescription.innerHTML = storeData[1].description;
-  popupsBootstrapLanguageDiv.classList.add('bootstrap-language-disappear');
-  popupsRubyLanguageDiv.classList.add('ruby-language-disappear');
-}
+  popupsProjectDesktop.innerHTML = storeData[1].aboutProjectDesktop;
+  popupsProjectMobile.innerHTML = storeData[1].aboutProjectMobile;
+  seeliveaTag.href = storeData[1].href;
+  popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
+  popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
+});
 
-function projectViewButton3() {
+const btnThree = document.querySelector('.btn-three');
+
+btnThree.addEventListener('click', () => {
   popups.style.display = 'flex';
   document.querySelector('#popups-project-img').src = storeData[2].imagesrc;
   popupProjectTitle.innerHTML = storeData[2].name;
-  popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
-  popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
-}
+  popupProjectDescription.innerHTML = storeData[2].description;
+  popupsBootstrapLanguageDiv.classList.add('bootstrap-language-disappear');
+  popupsRubyLanguageDiv.classList.add('ruby-language-disappear');
+});
 
-function projectViewButton4() {
+const btnFour = document.querySelector('.btn-four');
+
+btnFour.addEventListener('click', () => {
   popups.style.display = 'flex';
   document.querySelector('#popups-project-img').src = storeData[3].imagesrc;
   popupProjectTitle.innerHTML = storeData[3].name;
-  popupsBootstrapLanguageDiv.classList.add('bootstrap-language-disappear');
-  popupsRubyLanguageDiv.classList.add('ruby-language-disappear');
-}
+  popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
+  popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
+});
