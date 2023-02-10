11a7133405bb14308bbd80fa5e4d458a5fde9c75
unknown <you@example.com>
2023-02-10 22:22:40 +0300

    update files

--- app.js
+++ app.js
@@ -22,38 +22,38 @@ contact.addEventListener('click', () => {
   dh.classList.toggle('open');
 });
 
-
 const storeData = [
 
-{
-  name: 'Tonic',
-  description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
-  imagesrc: './image/Snapshoot Portfolio (1).svg" alt="content image',
-  technologies: ['html', 'css', 'javaScript'],
-  link: 'link_to_live_version.com',
-},
-{
-  name: 'Multi-post',
-  description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
-  imagesrc: './image/Snapshoot Portfolio (2).svg',
-  technologies: ['html', 'css', 'javaScript'],
-  link: 'link_to_live_version.com',
-},
-{
-  name: 'Multi-post',
-  description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
-  imagesrc: './image/Snapshoot Portfolio (3).svg" alt="content image',
-  technologies: ['html', 'css', 'javaScript'],
-  link: 'link_to_live_version.com',
-},
-{
-  name: 'Tonic',
-  description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
-  imagesrc: './image/Snapshoot Portfolio (4).svg',
-  technologies: ['html', 'css', 'javaScript'],
-  link: 'link_to_live_version.com',
-},
-]
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (1).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Multi-post',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (2).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Multi-post',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (3).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (4).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+];
+
 const description = document.querySelectorAll('.work-paragraph');
 for (let i = 0; i < description.length; i += 2) {
   description[i].innerHTML = storeData[i].description;
@@ -85,11 +85,11 @@ const popupsRubyLanguageDiv = document.querySelector('.ruby-language');
 const popupsProjectDesktop = document.querySelector('.popups-project-p-desktop');
 const popupsProjectMobile = document.querySelector('.popups-project-p-mobile');
 const seeliveaTag = document.querySelector('.see-live-aTag');
-let popupsProjectImage = document.querySelector('#popups-project-img')
+const popupsProjectImage = document.querySelector('#popups-project-img');
 
 function projectViewButton1() {
   popups.style.display = 'flex';
-  popupsProjectImage.src = storeData[0].imagesrc;
+  document.querySelector('#popups-project-img').src = storeData[0].imagesrc;
   popupProjectTitle.innerHTML = storeData[0].name;
   popupsProjectDesktop.innerHTML = storeData[0].aboutProjectDesktop;
   popupsProjectMobile.innerHTML = storeData[0].aboutProjectMobile;
@@ -100,7 +100,7 @@ function projectViewButton1() {
 
 function projectViewButton2() {
   popups.style.display = 'flex';
-  popupsProjectImage.src = storeData[1].imagesrc;
+  document.querySelector('#popups-project-img').src = storeData[1].imagesrc;
   popupProjectTitle.innerHTML = storeData[1].name;
   popupProjectDescription.innerHTML = storeData[1].description;
   popupsBootstrapLanguageDiv.classList.add('bootstrap-language-disappear');
@@ -109,7 +109,7 @@ function projectViewButton2() {
 
 function projectViewButton3() {
   popups.style.display = 'flex';
-  popupsProjectImage.src = storeData[2].imagesrc;
+  document.querySelector('#popups-project-img').src = storeData[2].imagesrc;
   popupProjectTitle.innerHTML = storeData[2].name;
   popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
   popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
@@ -117,9 +117,8 @@ function projectViewButton3() {
 
 function projectViewButton4() {
   popups.style.display = 'flex';
-  popupsProjectImage.src = storeData[3].imagesrc;
+  document.querySelector('#popups-project-img').src = storeData[3].imagesrc;
   popupProjectTitle.innerHTML = storeData[3].name;
   popupsBootstrapLanguageDiv.classList.add('bootstrap-language-disappear');
   popupsRubyLanguageDiv.classList.add('ruby-language-disappear');
 }
-
